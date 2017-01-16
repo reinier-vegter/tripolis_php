936a7f8c5b3009147b32d363aaf9605d2b2d0687
Tom van Vliet <tom.van.vliet@finalist.nl>
2017-01-16 11:53:10 +0100

    Add classes to retrieve workspaces and article types

---
+++ class/TripolisWorkspaceService.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @file
+ * Class to fetch workspaces.
+ */
+
+/**
+ * Class TripolisWorkspaceService.
+ */
+class TripolisWorkspaceService extends TripolisAPISoap {
+  /**
+   * Constructor function.
+   *
+   * @param string $client
+   *   Client name.
+   * @param string $username
+   *   Username to use for API.
+   * @param string $password
+   *   API password.
+   */
+  public function __construct($client, $username, $password) {
+    parent::__construct($client, $username, $password);
+    $this->setupSoap('WorkspaceService');
+  }
+
+  /**
+   * Retrieve all workspaces based on database ID.
+   *
+   * @param string $type
+   *   Group type.
+   *
+   * @return array|bool
+   *   Associative array containing merge of all paged requests, or FALSE
+   *   on failure.
+   */
+  public function getByContactDatabaseId($type = '') {
+    if (!empty($this->dbId)) {
+      try {
+        // Set Soap body params.
+        $param = array(
+          'getByContactDatabaseIdRequest' => array(
+            'contactDatabaseId' => $this->dbId,
+            'groupType' => $type,
+          ),
+        );
+        // Call API paged.
+        $result = $this->pagedSoapCall('getByContactDatabaseId', $param);
+
+        if (isset($result['workspaces']['workspace'])) {
+          return $result['workspaces']['workspace'];
+        }
+      }
+      catch (SoapFault $e) {
+        $this->throwError($e);
+      }
+    }
+
+    return FALSE;
+  }
+
+}
